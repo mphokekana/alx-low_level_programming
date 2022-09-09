9209c69a764884972c0aa96b98e4a9c7bf61ff3e
Mpho Kekana <mphokekana736@gmail.com>
2022-09-08 20:39:28 -0700

    print f'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the main function
+*
+* Description: using main function
+* this program prints "Programing is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the cutcome is a piece of art,\n");
+	return (0);
+}

d3c6859024762971ea8b30d1b21924de6a1f8cb0
Mpho Kekana <mphokekana736@gmail.com>
2022-09-09 06:12:30 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,10 @@
-
 #include <stdio.h>
-
 /**
- * main - Prints a text
- *
- * Return: Always 0 (Success)
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }

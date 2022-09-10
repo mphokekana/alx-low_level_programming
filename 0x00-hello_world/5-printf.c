dd7c931f032a6f7eb9dae09df9a2b4e56a219936
Mpho Kekana <mphokekana736@gmail.com>
2022-09-09 17:31:29 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,27 @@
-#includ <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
-* main - print the string in the put function
+* main - print if the number is positive, zero, or negative
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+* this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
 int main(void)
+n = rand() - RAND_MAX / 2;
+l - n % 10;
+
+if (l > 5)
 {
-	printf("with proper grammer, but the output is a piece of art,\n");
-	return (0);
+printf("Last digit of %d is %d and is greater than 5\n", n, l);
+}
+else if (l == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, l):
+}else
+}
+	print("Last digit of %d is %d and is less than 6 and not 0\n, n, l);
+}
+return (0);
 }

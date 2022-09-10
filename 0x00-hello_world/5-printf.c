fde4570b99dc72fa5ae2887bb0ad5d0c8b923629
Mpho Kekana <mphokekana736@gmail.com>
2022-09-10 02:35:02 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
-
+#include <time.h>
+#include <stdio.h>
 /**
- * main - print the string in the put function
+ * main - Pring random number to variable
  *
- * Description: using the main function
- * this program prints "Printing is like building a multilingual puzzle
- * Return: 0
- * */
+ * Return: Always (Successful)
+ */
 int main(void)
 {
-	printf("with proper grammar, but coutcome is a piece of art,\n");
+	int n;
+	char last[] = "Last digit of";
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%s %d is %d and is ", last, n, n % 10);
+	if (n % 10 > 5)
+	{
+		printf("greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printf("less than 6 and not 0\n");
+	}
 	return (0);
 }
 

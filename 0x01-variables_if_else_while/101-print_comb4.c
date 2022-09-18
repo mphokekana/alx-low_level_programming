3bf2304675e54e87ca2ca3a650363d8cdefe72ee
Mpho Kekana <mphokekana736@gmail.com>
2022-09-18 00:59:02 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print possible different combinations of three digits
+ * Description: using a function
+ * this program prints: "Programming is possible combination of three digits
+ * Return: 0
+ */
+int main(void)
+{
+int n1 = 0, n2, n3;
+
+while (n1 <= 9)
+{
+n2 = 0;
+while (n2 <= 9)
+{
+
+n3 = 0;
+while (n3 <= 9)
+{
+if (n1 != n2 && n1 < n2 && n2 != n3 && n2 < n3)
+{
+	putchar(n1 + 48);
+	putchar(n2 + 48);
+	putchar(n3 + 48);
+
+if (n1 + n2 + n3 != 24)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+++n3;
+}
+++n2;
+}
+++n1;
+}
+	putchar('\n');
+return (0);
+}

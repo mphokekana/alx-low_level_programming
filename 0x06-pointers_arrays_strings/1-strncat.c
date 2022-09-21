297493e1d4fa3ee8de9ce363caba91835a08db58
Mpho Kekana <mphokekana736@gmail.com>
2022-09-21 02:20:25 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strncat - concatenate tow strings but use inputted bytes
+ * @dest: the string to append appen
+ * @src: string to be complete at the end of the dest
+ * @n: integer to be compared to the index
+ * Return: returns new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index  = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}

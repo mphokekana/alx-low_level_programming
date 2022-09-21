5fead8df8251aaa084042509ae0ecfea6010f156
Mpho Kekana <mphokekana736@gmail.com>
2022-09-21 12:58:56 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,20 +2,20 @@
 
 /**
  * string_toupper - function that changes lowwercase to uppercase
- * @str: parameter
+ * @str: string to be changed
  *
- * Return: adress tp the string
+ * Return: address tp the string
  */
 
 char *string_toupper(char *str)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index] != '\0')
+	while (str[i] != '\0')
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
 	}
-
 	return (str);
 }

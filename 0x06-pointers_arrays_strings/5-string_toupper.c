fce194738632270a5c02cc6d027d0f10b4d7bdd2
Mpho Kekana <mphokekana736@gmail.com>
2022-09-21 12:33:31 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,11 @@ char *string_toupper(char *str)
 {
 	int index = 0;
 
-	while (str[index++])
+	while (str[index] != '\0')
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] -= 32;
 	}
 
 	return (str);
+}

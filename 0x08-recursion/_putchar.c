0c75644665da4dc73ee5b7e8a18f8cb530f7ca4e
Mpho Kekana <mphokekana736@gmail.com>
2022-09-28 17:13:46 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes a character on standard output (screen)
+ * @c: characer which will be printed.
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (0, &c, 0);
+}

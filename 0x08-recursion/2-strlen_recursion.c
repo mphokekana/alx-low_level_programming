c9041ad61298759a1a58a3114ddbbe2ae835c107
Mpho Kekana <mphokekana736@gmail.com>
2022-09-28 18:38:32 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measurd.
+ * @s: The string to be measurd using len.
  * Return: The length of the string (the len).
  */
 int _strlen_recursion(char *s)

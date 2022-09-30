8e93d022788422ad4b2b34d3ab356da8bd3453a0
Mpho Kekana <mphokekana736@gmail.com>
2022-09-30 14:42:22 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar - write the charater c to the standard output.
+ * @c: character to be printed.
+ * Return: 1 if succesfull and -1 if error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

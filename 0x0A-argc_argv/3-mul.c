e31ca27f39564e36449234444d7f053341a1f937
Mpho Kekana <mphokekana736@gmail.com>
2022-09-30 16:17:15 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,20 +4,20 @@
 
 /**
  * main - program that multiplies two numbers.
- * @argc: argument count
- * @argv: argument vertical
- * Return: 0 if success and 1 if error
+ * @argc: argument count.
+ * @argv: argument vertical.
+ * Return: 0 if success and 1 if error.
  */
 int main(int argc, char *argv[])
 {
-    int i, j;
-    if (argc == 3)
-    {
-        i = atoi(argv[2]);
-        j = atoi(argv[2]);
-        printf("%d\n", i * j);
-        return (0);
-    }
-    printf("error\n");
-    return (1);
+	int i, j;
+	if (argc == 3)
+	{
+		i = atoi(argv[2]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
+	return (0);
+	}
+	printf("error\n");
+	return (1);
 }

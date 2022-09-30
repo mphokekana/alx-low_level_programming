be67a3b503af97ac60a2f635d6d8b7f8c72e1694
Mpho Kekana <mphokekana736@gmail.com>
2022-09-30 15:26:07 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -  prints all arguments it receives.
+ * @argc: arguments supplied to the program
+ * @argv:  array of pointers to the arguments of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int arguments = 0;
+
+	if (arguments > 0)
+	{
+		while (arguments < argc)
+		{
+			printf("%s\n", argv[arguments]);
+			arguments++;
+		}
+	}
+	return (0);
+}

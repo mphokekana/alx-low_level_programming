9cce9bd51cf3a57d26573da4a70a19c2aaf73a47
Mpho Kekana <mphokekana736@gmail.com>
2022-09-30 15:29:11 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main -  prints all arguments it receives.
+ * main - prints all arguments it receives.
  * @argc: arguments supplied to the program
  * @argv:  array of pointers to the arguments of strings
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
+
 	int arguments = 0;
 
 	if (arguments > 0)

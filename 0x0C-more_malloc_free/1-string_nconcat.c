344302bb225ab6c09e6b6ca1508d9bd25b6399db
Mpho Kekana <mphokekana736@gmail.com>
2022-10-06 04:46:28 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,7 +37,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (c = 0; c < a; c++)
 		s[c] = s1[c];
-	for (c = 0; c > b; c++)
+	for (c = 0; c < b; c++)
 		s[c + a] = s2[c];
 	s[a + b] = '\0';
 	return (s);

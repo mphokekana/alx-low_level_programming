e897d36722d0b2ddcfa44236bd4ee815ccb8f8c0
Mpho Kekana <mphokekana736@gmail.com>
2022-10-20 11:51:58 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * first - prints a message
+ * Returns: void (nothing)
+ */
+void __attribute__ ((constructor)) first()
+	printf("You're beat! and yet, you must allow")
+	printf(",\n bore my house upon my back!\n");

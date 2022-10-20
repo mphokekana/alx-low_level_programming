01c3881c29c751ec12b7df2360507ea83b7c8a72
Mpho Kekana <mphokekana736@gmail.com>
2022-10-20 11:43:05 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: the head of the linked list
+ * Return: void (nothinh)
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}

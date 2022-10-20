ba5553b1b89fb221011079f234302135af4d9080
Mpho Kekana <mphokekana736@gmail.com>
2022-10-20 12:10:05 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,10 @@
 SECTION .data
-msg:	db "HELLO, Holberton", 0
+msg:	db "Hello, Holberton", 0
 fmt:	db "%s", 10, 0
 
 	SECTION .text
 	extern printf
-	global min
+	global main
 main:
 	mov esi, msg
 	mov edi, fmt

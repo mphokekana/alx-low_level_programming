25b83dad2d5ef30ec7777f5c08af05e6bddcfcdf
Mpho Kekana <mphokekana736@gmail.com>
2022-10-20 11:58:54 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@
  */
 void __attribute__ ((constructor)) first()
 {
-	printf("You're beat! and yet, you must allow")
+	printf("You're beat! and yet, you must allow");
 	printf(",\n bore my house upon my back!\n");
 }

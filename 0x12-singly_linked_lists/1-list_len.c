3f7afe06871b91324f532c8f5490526c38cb2aeb
Mpho Kekana <mphokekana736@gmail.com>
2022-10-20 08:01:16 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "listt.h"
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: single linked list
+ * Return:  returns the number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t numb_nodes;
+
+	numb_nodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		numb_nodes++;
+	}
+	return (numb_nodes);
+}

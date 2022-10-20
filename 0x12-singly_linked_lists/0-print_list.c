e6dc8aecb0865751837b551a9cb963932330b7ce
Mpho Kekana <mphokekana736@gmail.com>
2022-10-20 07:49:52 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,9 +6,9 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t list;
 
-	melem = 0;
+	list = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		list++;
 	}
-	return (nelem);
+	return (list);
 }

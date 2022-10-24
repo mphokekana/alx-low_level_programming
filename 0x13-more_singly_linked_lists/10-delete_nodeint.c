862785c7b66f1dd2f61b9de74a7526ac4f575d41
Mpho Kekana <mphokekana736@gmail.com>
2022-10-24 10:23:44 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		copy = copy->next;
 	}
 	temp = copy->next;
-	copy->next = tempnext;
+	copy->next = temp->next;
 	free(temp);
 	return (1);
 }

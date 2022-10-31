21f3754c50e39fe41b4556efcdbbb5771ceabb2b
Mpho Kekana <mphokekana736@gmail.com>
2022-10-31 06:17:36 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	for (NumbOfLetters = 0; text_content[NumbOfLetters]; NumbOfletters++)
 		;
-	wrt = write(fd, text_content, NumbOfLetters)
+	wrt = write(fd, text_content, NumbOfLetters);
 
 	if (wrt == -1)
 		return (-1);

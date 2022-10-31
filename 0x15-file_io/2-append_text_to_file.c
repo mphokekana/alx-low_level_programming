35135b4ea0b46a6f53ff68404b278c8886fddd4d
Mpho Kekana <mphokekana736@gmail.com>
2022-10-31 12:15:47 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,4 +8,25 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, NumbOfLetters, 
+	int fd, NumbOfLetters, wrt;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (NumbOfLetters = 0; text_content[NumbOfLetters]; NumbOfLetters++)
+			;
+		wrt = write(fd, text_content, NumbOfLetters);
+
+		if (wrt == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
